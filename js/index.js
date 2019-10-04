c824616ca759e1d466d1c6ba9d8996fa3dd68aab
Loginov Yura <yuraloginoff@gmail.com>
2019-10-04 21:39:57 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 "use strict"
 
-const mainTemplate = document.querySelector('#template')
+const mainTemplate = document.querySelector('#template').innerHTML
 const mainWrap = document.querySelector('#index')
 
 /**
  * Get all items from Chrome storage
+ *
  * @returns Promise
  */
 function findAll() {
@@ -13,13 +14,23 @@ function findAll() {
   )
 }
 
-function renderTemplate(data, elem) {
-  const template = elem.innerHTML
-  Mustache.parse(template)
-  const rendered = Mustache.render(template, data)
-  mainWrap.innerHTML = rendered
+
+/**
+ * Renders Mustache template using data from chrome storage
+ *
+ * @param {JSON} data
+ * @param {string} templateSrc
+ * @param {Node} outputElem
+ */
+function renderTemplate(data, templateSrc, outputElem) {
+  Mustache.parse(templateSrc)
+  outputElem.innerHTML = Mustache.render(templateSrc, data)
 }
 
+
+/**
+ * Gets data prmise and renders demplate
+ */
 findAll().then(
-  result => renderTemplate(result, mainTemplate)
+  result => renderTemplate(result, mainTemplate, mainWrap)
 )
