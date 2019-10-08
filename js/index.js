93b98903c49c4d8e4f9b33f63085bba121da465b
Loginov Yura <yuraloginoff@gmail.com>
2019-10-08 19:18:21 +0300

    + magic grid

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ import * as db  from "./db.js";
 
 const mainTemplate = document.querySelector('#template').innerHTML
 const mainWrap = document.querySelector('#index')
+let magicGrid
 
 /**
  * Renders Mustache template using data from chrome storage
@@ -21,5 +22,18 @@ function renderTemplate(data, templateSrc, outputElem) {
  * Gets data prmise and renders demplate
  */
 db.findAll().then(
-  result => renderTemplate(result, mainTemplate, mainWrap)
+  result => {
+    renderTemplate(result, mainTemplate, mainWrap)
+    magicGrid = new MagicGrid({
+      container: ".wrap", // Required. Can be a class, id, or an HTMLElement
+      static: true, // Required for static content. Default: false.
+      gutter: 30, // Optional. Space between items. Default: 25(px).
+      maxColumns: 10, // Optional. Maximum number of columns. Default: Infinite.
+      useMin: true, // Optional. Prioritize shorter columns when positioning items. Default: false.
+      useTransform: true, // Optional. Position items using CSS transform. Default: True.
+      animate: false // Optional. Animate item positioning. Default: false.
+    })
+
+    magicGrid.listen()
+  }
 )
