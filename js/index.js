1fb65d27dbe5f15a6b2d6f298fbbb1381d0970ba
Loginov Yura <yuraloginoff@gmail.com>
2019-10-11 14:21:54 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,8 +4,16 @@ import * as db  from "./db.js";
 
 const mainTemplate = document.querySelector('#template').innerHTML
 const mainWrap = document.querySelector('#index')
-let magicGrid
-
+const gridSettings = {
+  container: ".wrap", // Required. Can be a class, id, or an HTMLElement
+  static: true, // Required for static content. Default: false.
+  gutter: 30, // Optional. Space between items. Default: 25(px).
+  maxColumns: 10, // Optional. Maximum number of columns. Default: Infinite.
+  useMin: true, // Optional. Prioritize shorter columns when positioning items. Default: false.
+  useTransform: true, // Optional. Position items using CSS transform. Default: True.
+  animate: false // Optional. Animate item positioning. Default: false.
+}
+let grid
 /**
  * Renders Mustache template using data from chrome storage
  *
@@ -26,15 +34,7 @@ db.findAll().then(
     renderTemplate(result, mainTemplate, mainWrap)
 
     // masonry layout
-    magicGrid = new MagicGrid({
-      container: ".wrap", // Required. Can be a class, id, or an HTMLElement
-      static: true, // Required for static content. Default: false.
-      gutter: 30, // Optional. Space between items. Default: 25(px).
-      maxColumns: 10, // Optional. Maximum number of columns. Default: Infinite.
-      useMin: true, // Optional. Prioritize shorter columns when positioning items. Default: false.
-      useTransform: true, // Optional. Position items using CSS transform. Default: True.
-      animate: false // Optional. Animate item positioning. Default: false.
-    })
-    magicGrid.listen()
+    grid = new MagicGrid(gridSettings)
+    grid.listen()
   }
 )
